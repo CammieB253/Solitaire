2ab054912e50ae9f2a9b44f91d87e0a3b4bf60fb
Dionysus9517 <cameronbuchan395@gmail.com>
2023-05-12 19:17:06 +0100

    base code

---
+++ Cards.java
@@ -0,0 +1,54 @@
+public class Cards {
+
+    public static void originalDeck() {
+
+        // fill an array with a deck of cards, either hard coded card generation or CSV file
+        // Rank, Suit, Colour can be determined from suit through another method
+        // Examples of cards represented in data: 1C (Ace, Clubs), 5H (5, Hearts), 14 (empty space), 12S (Queen, Spades)
+
+        String[] mainDeck = {};
+
+        int i = 0;
+        int rank = 0;
+        String suit = "A";
+
+        for (i = 0; i == 52; i++) {
+
+            int counter = 0;
+
+            if (counter > 14) {
+                counter = 0;
+            }
+
+            rank = counter + 1;
+            counter++;
+
+
+            if (i >= 14 && i <= 26) {
+                suit = "S";
+            } else if (i >= 28 && i <= 39) {
+                suit = "H";
+            } else if (i >= 40 && i <= 52) {
+                suit = "D";
+            }
+
+            mainDeck[i] = (rank + suit);
+
+        }
+
+        for (int x = 0; x >= mainDeck.length;) {
+            System.out.println(mainDeck) ;
+        }
+
+
+    }
+
+
+
+
+
+
+    // method to shuffle the previous array - random number generation, fill a new array with data from last array
+
+
+}
